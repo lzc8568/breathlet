1f84d7515ee92a18ef9d065b5e19a886e3a7e0e8
zhichuangli <zhichuang.li@shopee.com>
2026-05-21 10:10:21 +0800

    Use native MenuBarExtra for menu

--- TakeABreak/TakeABreakApp.swift
+++ TakeABreak/TakeABreakApp.swift
@@ -3,8 +3,23 @@ import SwiftUI
 @main
 struct TakeABreakApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    @StateObject private var controller: BreakController
+
+    init() {
+        _controller = StateObject(wrappedValue: BreakController(preferences: Preferences.shared))
+    }
 
     var body: some Scene {
+        MenuBarExtra {
+            MenuBarContentView()
+                .environmentObject(controller)
+        } label: {
+            HStack(spacing: 4) {
+                Image(systemName: "cup.and.saucer")
+                Text(controller.menuBarTitle)
+            }
+        }
+
         Settings {
             PreferencesView()
                 .environmentObject(Preferences.shared)
@@ -14,83 +29,47 @@ struct TakeABreakApp: App {
 
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusItem: NSStatusItem?
-    private var controller: BreakController?
-    private var menuUpdateTimer: Timer?
-    private var stateMenuItem: NSMenuItem?
-
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
+    }
+}
 
-        let preferences = Preferences.shared
-        let controller = BreakController(preferences: preferences)
-        self.controller = controller
+private struct MenuBarContentView: View {
+    @EnvironmentObject private var controller: BreakController
 
-        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        self.statusItem = statusItem
-        statusItem.button?.image = NSImage(systemSymbolName: "cup.and.saucer", accessibilityDescription: "Take a Break")
-        statusItem.button?.imagePosition = .imageLeading
+    var body: some View {
+        Text(controller.isBreakActive ? "Break is running" : "Next break in \(controller.menuBarTitle)")
 
-        configureMenu()
-        updateMenuBarTitle()
+        Divider()
 
-        menuUpdateTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
-            MainActor.assumeIsolated {
-                self?.updateMenuBarTitle()
-                self?.updateStateMenuItem()
-            }
+        Button("Take Break Now") {
+            controller.triggerBreakNow()
         }
+        .keyboardShortcut("b")
+
+        Button("Reset Timer") {
+            controller.resetWorkTimer()
+        }
+        .keyboardShortcut("r")
+
+        Divider()
+
+        Button("Preferences...") {
+            openSettings()
+        }
+        .keyboardShortcut(",")
+
+        Divider()
+
+        Button("Quit Take a Break") {
+            NSApp.terminate(nil)
+        }
+        .keyboardShortcut("q")
     }
 
-    private func updateMenuBarTitle() {
-        statusItem?.button?.title = " \(controller?.menuBarTitle ?? "Take a Break")"
-    }
-
-    private func configureMenu() {
-        let menu = NSMenu()
-
-        let stateItem = NSMenuItem(title: "", action: nil, keyEquivalent: "")
-        stateItem.isEnabled = false
-        stateMenuItem = stateItem
-        menu.addItem(stateItem)
-        menu.addItem(.separator())
-
-        menu.addItem(menuItem(title: "Take Break Now", action: #selector(takeBreakNow), keyEquivalent: "b"))
-        menu.addItem(menuItem(title: "Reset Timer", action: #selector(resetTimer), keyEquivalent: "r"))
-        menu.addItem(.separator())
-        menu.addItem(menuItem(title: "Preferences...", action: #selector(openSettings), keyEquivalent: ","))
-        menu.addItem(.separator())
-        menu.addItem(menuItem(title: "Quit Take a Break", action: #selector(quit), keyEquivalent: "q"))
-
-        statusItem?.menu = menu
-        updateStateMenuItem()
-    }
-
-    private func updateStateMenuItem() {
-        stateMenuItem?.title = controller?.isBreakActive == true ? "Break is running" : "Next break in \(controller?.menuBarTitle ?? "--:--")"
-    }
-
-    private func menuItem(title: String, action: Selector, keyEquivalent: String) -> NSMenuItem {
-        let item = NSMenuItem(title: title, action: action, keyEquivalent: keyEquivalent)
-        item.target = self
-        return item
-    }
-
-    @objc private func takeBreakNow() {
-        controller?.triggerBreakNow()
-    }
-
-    @objc private func resetTimer() {
-        controller?.resetWorkTimer()
-    }
-
-    @objc private func openSettings() {
+    private func openSettings() {
         NSApp.setActivationPolicy(.regular)
         NSApp.activate(ignoringOtherApps: true)
         NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
-    }
-
-    @objc private func quit() {
-        NSApp.terminate(nil)
     }
 }
